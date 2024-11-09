91167e0f1995d6baaaf4a19743d83f0cd76257c1
Nii-khil <nikhilupadhyayblr@gmail.com>
2024-11-10 00:57:21 +0530

    Login using function, Add .sql file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import PermitPage from "./Permit";
 import ParkingViolation from "./ParkingViolation";
 import ReserveSpot from "./ReserveSpot";
 import AdminDashboard from "./AdminDashboard";
-// import UserDashboardWelcome from "./UserDashboardWelcome";
+import UserDashboardWelcome from "./UserDashboardWelcome";
 import { UserProvider } from "./UserContext";
 
 function App() {
@@ -30,21 +30,6 @@ function App() {
         />
       </Router>
     </UserProvider>
-  );
-}
-
-function UserDashboardWelcome() {
-  return (
-    <div className="text-center p-8 bg-gray-900 shadow-md">
-      <h2 className="text-3xl font-bold text-white mb-4">Welcome to the Campus Parking Management System</h2>
-      <p className="text-gray-300 mb-4">
-        As a user, you have access to various features that make parking on campus easier and more efficient. 
-        You can view real-time parking availability, check the status of your parking permit, and even reserve a parking spot.
-      </p>
-      <p className="text-gray-400">
-        Use the navigation menu to explore these options and manage your parking needs. 
-      </p>
-    </div>
   );
 }
 
